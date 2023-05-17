c69970a86b500020c2b659d993a273a039f8e080
Manuel Beras <manuel.beras@gmail.com>
2023-05-17 06:49:55 -0400

    Created Villa Model Wrapper

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController: ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas() 
+        { 
+            return new List<VillaDTO> { 
+                new VillaDTO{Id=1, Name="Pool View" },
+                new VillaDTO{Id=2, Name="Beach View" }
+            };
+        }
     }
 }
